beb4ef97595b673f3b102c8bed6e93bdf00533f8
heintz <heintzrboerje@proton.me>
2025-04-27 18:56:54 +0200

    Petit ajustement

--- battleship.ml
+++ battleship.ml
@@ -296,8 +296,7 @@ let rec auto_placing_ships(p_ships, p_grid, p_param : (string * int) list * t_gr
   else
     let l_pos_x : char = char_of_int(int_of_char('A') + Random.int(Array.length(p_grid)))
     and l_pos_y : int = Random.int(Array.length(p_grid))
-    (* and l_direction : char = [|'u'; 'd'; 'l'; 'r'|].(Random.int(4)) *)
-    and l_direction : char = [|'l'; 'r'; 'u'; 'd'|].(Random.int(2))
+    and l_direction : char = [|'u'; 'd'; 'l'; 'r'|].(Random.int(4))
     in
     let l_current_ship : t_ship = {name = fst(List.hd(p_ships)); positions = positions_list((l_pos_x, l_pos_y), l_direction, snd(List.hd(p_ships)))}
     in
